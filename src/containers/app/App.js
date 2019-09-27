73671f90c1e652ebb4a972a2cf906c5e8faff67b
Kevin Kabore <kevin@tvgla.com>
2019-09-27 13:17:41 -0700

    reinitialize files

---
+++ src/containers/app/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.styles.css';
+
+import { HomePage } from '../../HomePage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      This is the app component
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
