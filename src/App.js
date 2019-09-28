8bda6a118957347ab82519076183ca565a669218
Kevin Kabore <kevin.s.kabore@gmail.com>
2019-09-27 18:05:27 -0700

    Adds react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './app.styles.css';
 
 import HomePage from './pages/homepage/HomePage';
@@ -8,7 +10,7 @@ import HomePage from './pages/homepage/HomePage';
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path="/" component={HomePage}/> 
     </div>
   );
 }
