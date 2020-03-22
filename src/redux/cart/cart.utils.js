3351e7b7e09a889d1bed935799fe7bb77086f1fe
Kevin Kabore <kevin.s.kabore@gmail.com>
2020-03-22 01:06:55 -0700

    feat(cartItems): add remove and add from Cart Item

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,3 +16,20 @@ export const addItemToCart = (cartItems, newCartItem) => {
   // else add and set quantity to 1
   return [...cartItems, { ...newCartItem, quantity: 1 }];
 };
+
+export const removeCartItem = (cartItems, cartItemToRemove) => {
+  console.log('cartItems:', cartItems);
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
